6b51dcd4908a5971d4d08cfbca91500fc661dc6c
Anthony Bloomer <ant0@protonmail.ch>
2017-02-04 17:40:29 +0000

    Output an error message if the user has not input the required arguments
    Fix indentation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-var username = process.argv[2];
-var playlistid = process.argv[3];
+if (process.argv.length >= 4){
+  var username = process.argv[2];
+  var playlistid = process.argv[3];
+} else {
+  console.error('spodl requires 2 arguments. The spotify username and playlist url.');
+}
+
 var config = require('../config.js');
 var SpotifyWebApi = require('spotify-web-api-node');
 var YouTube = require('youtube-node');
